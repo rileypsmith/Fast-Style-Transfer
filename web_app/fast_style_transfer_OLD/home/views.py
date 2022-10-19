80dda95dae0966d718250f84d0cddaeca6897529
Riley Smith <smith.rileyp@gmail.com>
2022-10-18 21:21:38 -0700

    Removing some old files from older version of web app

--- web_app/fast_style_transfer_OLD/home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Image
-from .serializers import ImageSerializer
-from rest_framework import generics
-
-# Create your views here.
-class ImageListCreate(generics.ListCreateAPIView):
-    queryset = Image.objects.all()
-    serializer_class = ImageSerializer
